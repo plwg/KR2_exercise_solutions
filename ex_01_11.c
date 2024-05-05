e0f2f0ef8107049932ac4eefbe8a6cc6fc94b24d
Paul <56353330+plwg@users.noreply.github.com>
2024-05-05 23:01:24 +0800

    solved 01-11

---
+++ ex_01_11.c
@@ -0,0 +1,35 @@
+/*
+1. Tested zero lenght input
+2. Tested moby dick
+
+*/
+#include <stdio.h>
+
+#define IN 1  /* inside a word */
+#define OUT 0 /* outside a word */
+
+/* counts lines, words and characters as input */
+
+int main()
+{
+    unsigned int c, nl, nw, nc, state;
+
+    state = OUT;
+    nl = nw = nc = 0;
+    while ((c = getchar()) != EOF)
+    {
+        ++nc;
+        if (c == '\n')
+            ++nl;
+        if (c == ' ' || c == '\n' || c == '\t')
+            state = OUT;
+        else if (state == OUT)
+        {
+            state = IN;
+            ++nw;
+        }
+    }
+    printf("%d %d %d\n", nl, nw, nc);
+
+    return 0;
+}
