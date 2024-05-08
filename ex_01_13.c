4f64e8cbb3568add6b5f2b9076b1b8f7eec16cdd
Paul <56353330+plwg@users.noreply.github.com>
2024-05-08 21:42:29 +0800

    solved 01_13

---
+++ ex_01_13.c
@@ -0,0 +1,126 @@
+#include <stdio.h>
+#include <ctype.h>
+#define IN 1
+#define OUT 0
+#define NBIN 20
+#define BIN_SIZE 1
+#define MAX_WIDTH 20
+#define MAX_HEIGHT 20
+
+void print_horizontal_histogram(int stat[], int star_size);
+void print_vertical_histogram(int stat[], int height, int star_size);
+
+int main()
+{
+    int c;
+    int stat[NBIN] = {0};
+    int status = OUT;
+    int count, max;
+    int bin;
+    int height;
+
+    count = max = 0;
+
+    while ((c = getchar()) != EOF)
+    {
+        if (c == '\n' || c == ' ' || c == '\t' || ispunct(c) || isdigit(c))
+        {
+            if (status == IN)
+            {
+                bin = count / BIN_SIZE;
+
+                if (bin > NBIN)
+                {
+                    bin = NBIN;
+                }
+
+                stat[bin - 1] = stat[bin - 1] + 1;
+            }
+            count = 0;
+            status = OUT;
+        }
+        else
+        {
+            status = IN;
+            count++;
+        }
+    }
+
+    for (int i = 0; i < NBIN; i++)
+    {
+        if (stat[i] > max)
+        {
+            max = stat[i];
+        }
+    }
+
+    height = max > MAX_HEIGHT ? MAX_HEIGHT : max;
+
+    print_horizontal_histogram(stat, max / MAX_WIDTH + 1);
+    printf("\n\n\n\n\n");
+    print_vertical_histogram(stat, height, max / height + 1);
+
+    return 0;
+}
+
+void print_horizontal_histogram(int stat[], int star_size)
+{
+
+    printf("Horizontal Histogram\n");
+    for (int i = 0; i < NBIN; i++)
+    {
+        if (i < NBIN - 1)
+        {
+            printf("%10d: ", i + 1);
+        }
+        else
+        {
+            printf("%d and %d+: ", NBIN, NBIN);
+        }
+        for (int foo = stat[i]; foo > 0; foo -= star_size)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+
+void print_vertical_histogram(int stat[], int height, int star_size)
+{
+    printf("Vertical Histogram\n");
+    for (int i = height - 1; i >= 1; i--)
+    {
+        printf("|");
+        for (int j = 0; j < NBIN; j++)
+        {
+            if (stat[j] >= (i * star_size))
+            {
+                printf(" *|");
+            }
+            else
+            {
+                printf("  |");
+            }
+        }
+        printf("\n");
+    }
+    printf("|");
+    for (int j = 0; j < NBIN; j++)
+    {
+        if (stat[j] > 0)
+        {
+            printf(" *|");
+        }
+        else
+        {
+            printf("  |");
+        }
+    }
+    printf("\n");
+    printf(" "); // Align the labels with the bars
+    for (int j = 0; j < NBIN; j++)
+    {
+        printf("%2d ", j + 1); 
+    }
+    printf("\n");
+}
