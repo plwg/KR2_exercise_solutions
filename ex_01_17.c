0e02eff665911fea1bd39a02c4de19d6feeffc0d
Paul <56353330+plwg@users.noreply.github.com>
2024-05-12 23:26:53 +0800

    solved 01-17

---
+++ ex_01_17.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+    bool to_print = false;
+    int c;
+    int i;
+    int count = 0;
+    int bound;
+    const int MAXLINE = 9999;
+    const int MAX = 80;
+    char line[MAXLINE];
+
+    while (true)
+    {
+        c = getchar();
+
+        if (count < MAXLINE)
+        {
+            line[count] = c;
+        }
+
+        if (c != '\n' && c != EOF)
+        {
+            if (count == MAX - 1)
+            {
+                to_print = true;
+            }
+            count++;
+        }
+        else
+        {
+            if (to_print)
+            {
+                bound = count < (MAXLINE - 1) ? count : (MAXLINE - 1);
+                if (line[bound] == '\n')
+                {
+                    printf("%s", line);
+                }
+                else
+                {
+                    printf("%s\n", line);
+                }
+                to_print = false;
+                for (i = MAX; i <= bound; i++)
+                {
+                    line[i] = 0;
+                }
+            }
+
+            if (c == EOF)
+            {
+                break;
+            }
+
+            count = 0;
+        }
+    }
+    return 0;
+}
