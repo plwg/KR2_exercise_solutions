069d856d68b5c5ee6755c1e842acb1ea02700994
Paul <56353330+plwg@users.noreply.github.com>
2024-05-22 21:37:57 +0800

    solve 02-08

---
+++ ex_02_08.c
@@ -0,0 +1,39 @@
+#include "print_binary.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned rightrot(unsigned x, unsigned n);
+
+int main()
+{
+    unsigned x = (~0 << 3);
+
+    rightrot(x, 40);
+
+    return 0;
+}
+
+unsigned rightrot(unsigned x, unsigned n)
+{
+    int size_n = sizeof(n) * 8;
+    n %= size_n;
+    unsigned mask = ~((~0) << n);
+    unsigned res;
+
+    printf("%30s", "X: ");
+    print_binary(x);
+
+    printf("%30s", "MASK: ");
+    print_binary(mask);
+
+    printf("%30s", "MASKED X: ");
+    print_binary(mask & x);
+
+    printf("%30s", "MASKED X SHIFTED RIGHT: ");
+    print_binary((mask & x) << (size_n - n));
+
+    printf("%30s", "RESULT: ");
+    print_binary(res = ((mask & x) << (size_n - n) | (x >> n)));
+
+    return res;
+}
